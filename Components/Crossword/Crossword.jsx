3c7d912b60d5786c1ecfe647cd04e72ec4989146
Matt Poole <irishpanda123@Gmail.com>
2024-06-09 17:13:25 +0100

    select cells background colour

--- Components/Crossword/Crossword.jsx
+++ Components/Crossword/Crossword.jsx
@@ -5,12 +5,34 @@ import styles from './crossword.module.css'
 import CrosswordLetter from '@/Components/CrosswordLetters/CrosswordLetter'
 
 const Crossword = () => {
-    //Focus Change updates letter grid and moves focus to next letter. Key helps sort, grid is hor or vert.
-    const focusChange = (letterObj, key, grid) => {
+    //Stores currently selected cell and this with gridOrient infrom Question to show
+    const [currentCell, setCurrentCell] = useState(0);
+    //False for horizontal question, true for vertical question
+    const [gridOrient, setGridOrient] = useState(false);
+    //Focus Change updates letter grid and moves focus to next letter when typed. grid is hor or vert.
+    const cellChange = (key) => {
+        setCurrentCell(key);
+        setLetterGrid(letterGrid.map(obj => {
+            if(obj.key == key){
+                obj.focus = true
+                return obj
+            } else{
+                obj.focus = false
+                return obj
+            }
+        }));
+    }
+    //Called when letter is set and move automatically to next cell
+    const focusChange = (letterObj) => {
         const nextLetterGrid = letterGrid.map(obj => {
-            if(obj.key != key){
-                if(obj.key == key+1){
+            if(obj.key != letterObj.key){
+                //Set Orient Increment:
+                if(obj.key == letterObj.key+1 && !gridOrient){
                     obj.focus = true;
+                    setCurrentCell(obj.key);
+                } else if(obj.key == letterObj.key+5 && gridOrient){
+                    obj.focus = true;
+                    setCurrentCell(obj.key);
                 }
                 return obj
             } else{
@@ -22,13 +44,29 @@ const Crossword = () => {
         setLetterGrid(nextLetterGrid);
         return true;
     }; 
+    //Function to swith between hor and vert
+    const onGridHandler = () => setGridOrient(!gridOrient);
 
+    //Currently stores crossword grid, this will be moved to API
     const [letterGrid, setLetterGrid] = useState([{'letter':'w', 'key':0, 'focus': false},{'letter':'e', 'key':1, 'focus': false},{'letter':'i', 'key':2, 'focus': false},{'letter':'r', 'key':3, 'focus': false},{'letter':'d', 'key':4, 'focus': false},{'letter':'w', 'key':5, 'focus': false},{'letter':'e', 'key':6, 'focus': false},{'letter':'i', 'key':7, 'focus': false},{'letter':'r', 'key':8, 'focus': false},{'letter':'d', 'key':9, 'focus': false},{'letter':'w', 'key':10, 'focus': false},{'letter':'e', 'key':11, 'focus': false},{'letter':'i', 'key':12, 'focus': false},{'letter':'r', 'key':13, 'focus': false},{'letter':'d', 'key':14, 'focus': false},{'letter':'w', 'key':15, 'focus': false},{'letter':'e', 'key':16, 'focus': false},{'letter':'i', 'key':17, 'focus': false},{'letter':'r', 'key':18, 'focus': false},{'letter':'d', 'key':19, 'focus': false},{'letter':'w', 'key':20, 'focus': false},{'letter':'00', 'key':21, 'focus': false},{'letter':'i', 'key':22, 'focus': false},{'letter':'r', 'key':23, 'focus': false},{'letter':'d', 'key':24, 'focus': false}]);
-
+    
+    //Calculate grid level to select horizontal or vertical cells background
+    if(gridOrient){
+        var currentCellGridLevel = currentCell % 5;
+    } else{
+        var currentCellGridLevel = Math.floor(currentCell/5);
+    }
     return (
         <div className={styles.container}>
             {letterGrid.map((letterObj, index)=>{
-                return(<div className={styles.crossword_letter} key={index}><CrosswordLetter letterObj={letterObj} focusChange={focusChange}/></div>)
+                if(gridOrient){
+                    var objCellGridLevel = letterObj.key % 5;
+                } else{
+                    var objCellGridLevel = Math.floor(letterObj.key/5);
+                }
+                var backgroundColor = ``;
+                (letterObj.key == currentCell || objCellGridLevel === currentCellGridLevel) ? backgroundColor = `${styles.selectBackground} ${styles.crossword_letter}` : backgroundColor = `${styles.crossword_letter}`;
+                return(<div className={backgroundColor} key={index}><CrosswordLetter letterObj={letterObj} focusChange={focusChange} cellChange={cellChange} onGridHandler={onGridHandler}/></div>)
             })}
         </div>
     )
--- Components/CrosswordLetters/CrosswordLetter.jsx
+++ Components/CrosswordLetters/CrosswordLetter.jsx
@@ -5,6 +5,9 @@ import styles from './crosswordletters.module.css'
 
 const CrosswordLetter = (props) => {
     const [letterObj, setLetterObj] = useState(props.letterObj)
+    const selectHandler = () => {
+        props.cellChange(letterObj.key)
+    }
     const autoFocus = (element) => {
         if(letterObj.focus){
             element?.focus()
@@ -14,7 +17,7 @@ const CrosswordLetter = (props) => {
         if(letter.target.value.length == 1){
             setLetterObj({'letter':letter.target.value, 'key':letterObj.key, 'focus': letterObj.focus});
             //Move focus to next crossword letter block
-            props.focusChange(letterObj, letterObj.key, false);
+            props.focusChange({'letter':letter.target.value, 'key':letterObj.key, 'focus': false});
         } else if(letter.target.value.length == 0){
             setLetterObj({'letter':letter.target.value, 'key':letterObj.key, 'focus': letterObj.focus});
         }
@@ -26,7 +29,7 @@ const CrosswordLetter = (props) => {
         <div className={styles.container}>
             {letterObj.letter != '00' ? 
             <form className={styles.letterButton} onSubmit={submitHandler}>
-                <input ref={autoFocus} type="text" value={letterObj.letter} onChange={changeLetter} className={styles.input}></input>
+                <input ref={autoFocus} onClick={selectHandler} onDoubleClick={props.onGridHandler} type="text" value={letterObj.letter} onChange={changeLetter} className={styles.input}></input>
             </form>
             :
             <div className={styles.blackBox}></div>
