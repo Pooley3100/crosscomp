92888e741f9de5c303124908f32a7ed9043e4285
Matt Poole <irishpanda123@Gmail.com>
2024-06-14 22:11:50 +0100

    width options

--- Components/Question/Question.jsx
+++ Components/Question/Question.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 import styles from './question.module.css'
+import { useWindowSize } from "@uidotdev/usehooks";
 
 const Question = (props) => {
+  const size = useWindowSize();
+
   return (
-    <div className={styles.container}><div className={styles.questionBox}><p>{props.question}</p></div></div>
+    <div className={styles.container}>
+      {size.width < 700 ?
+      <div className={styles.questionBox}>
+        <p className={styles.questionText}>{props.question}</p>
+      </div>
+      :
+      <>
+      <div className={styles.questionListHorizontal}>
+        <p>Test</p>
+      </div>
+      <div className={styles.questionListVertical}>
+      <p>Test</p>
+      </div>
+      </>
+      }
+    </div>
   )
 }
 
