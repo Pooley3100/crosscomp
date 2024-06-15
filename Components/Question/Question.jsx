05a4ad76f2118ee4012b86ad3013eda90e832f1f
Matt Poole <irishpanda123@Gmail.com>
2024-06-15 22:38:25 +0100

    sidebar Qs

--- Components/Question/Question.jsx
+++ Components/Question/Question.jsx
@@ -8,18 +8,26 @@ const Question = (props) => {
   return (
     <div className={styles.container}>
       {size.width < 700 ?
-      <div className={styles.questionBox}>
-        <p className={styles.questionText}>{props.question}</p>
-      </div>
-      :
-      <>
-      <div className={styles.questionListHorizontal}>
-        <p>Test</p>
-      </div>
-      <div className={styles.questionListVertical}>
-      <p>Test</p>
-      </div>
-      </>
+        <div className={styles.questionBox}>
+          <p>{props.question}</p>
+        </div>
+        :
+        <>
+          <div className={styles.questionListHorizontal}>
+            <ul className={styles.questionList}>
+              <li className={styles.questionText}>
+                1. Salty
+              </li>
+            </ul>
+          </div>
+          <div className={styles.questionListVertical}>
+          <ul className={styles.questionList}>
+              <li className={styles.questionText}>
+                1. Salty
+              </li>
+            </ul>
+          </div>
+        </>
       }
     </div>
   )
